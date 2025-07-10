586bbeccae65adc94fadcdcc02e76f278b3f3367
mathis-fournier <mathis.fournier@laplateforme.io>
2025-07-10 14:47:30 +0200

    nombres premier

--- jour01/job02/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-    echo"Hello LaPlateforme"; /* COMMENTAIRE 
-                                 SUR PLUSIEUR LIGNES */
-    echo "Hello LaPlateforme"; // COMMENTAIRE 1 LIGNE ou # COMMENTAIRE
-    
-?>
