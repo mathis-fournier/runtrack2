56950d08e37ba8de1d24c3a42d72d8f783f0f0be
mathis-fournier <mathis.fournier@laplateforme.io>
2025-07-10 15:19:15 +0200

    rectangle

--- jour02/job06/index.php
+++ jour02/job06/index.php
@@ -1,11 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div></div>
-</body>
-</html>
+<?php
+
+$hauteur = 10;
+$largeur = 20;
+$handle = imagecreatetruecolor($largeur, $hauteur); 
+
+$green = imagecolorallocate($handle, 0, 255, 0); 
+
+imagefilledrectangle($handle, 0, 0, 640, 480, $green);
+
+header("Content-type: image/jpg");
+imagejpeg($handle);
+
+?>
