f3e64b881d1cc2ea162ea52dbd898b20cac605ce
mathis-fournier <mathis.fournier@laplateforme.io>
2025-07-22 11:26:57 +0200

    Update index.php

--- jour07/job07/index.php
+++ jour07/job07/index.php
@@ -54,12 +54,7 @@ function plateforme ($wc)
 
 
 function cesar ($str, $dec=2) {
-    $alphabet = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g',
-    'h', 'i', 'j', 'k', 'l', 'm', 'n',
-    'o', 'p', 'q', 'r', 's', 't', 'u',
-    'v', 'w', 'x', 'y', 'z' ];
-
+    $alphabet = range("a","z") ;
 
     for ($i = 0; $i < strlen($str); $i++) {
         $keywithdecalage = array_search($str[$i], $alphabet) + $dec;
