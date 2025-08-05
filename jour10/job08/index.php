442c7ebc1338948614b0aca3ca0c6a6ae16f200d
mathis-fournier <mathis.fournier@laplateforme.io>
2025-08-05 09:48:21 +0200

    job08 fini

---
+++ jour10/job08/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$servname = "localhost"; $user = "root"; $pass = ""; $dbname = "jour09";
+
+$conn = new mysqli($servname, $user, $pass, $dbname);
+$sql = "SELECT SUM(capacite) AS capacite_totale FROM salles";
+$result = $conn->query($sql);
+?>
+    <table>
+        <tr >
+            <th>Capacite Totale</th>
+        </tr>
+        <?php
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row["capacite_totale"] . "</td>";
+                echo "</tr>";
+            }
+        } else {
+            echo "<tr><td colspan='5'>Aucun résultat trouvé.</td></tr>";
+        }
+        ?>
+    </table>
+    <style>
+    th {
+        border: 1px solid black;
+        padding: 5px;
+    }
+</style>
+</body>
+</html>
