b39b23e1b15d41aec7a489a67936f06e76e0a6c0
mathis-fournier <mathis.fournier@laplateforme.io>
2025-08-05 09:30:13 +0200

    job04fini

---
+++ jour10/job04/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$servname = "localhost"; $user = "root"; $pass = ""; $dbname = "jour09";
+
+$conn = new mysqli($servname, $user, $pass, $dbname);
+$sql = "SELECT * FROM etudiants WHERE prenom LIKE 't%'";
+$result = $conn->query($sql);
+?>
+    <table>
+        <tr >
+            <th>Nom</th>
+            <th>Capacité</th>
+            <th>Naissance</th>
+            <th>Sexe</th>
+            <th>Email</th>
+        </tr>
+        <?php
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row["nom"] . "</td>";
+                echo "<td>" . $row["prenom"] . "</td>";
+                echo "<td>" . $row["naissance"] . "</td>";
+                echo "<td>" . $row["sexe"] . "</td>";
+                echo "<td>" . $row["email"] . "</td>";
+                echo "</tr>";
+            }
+        } else {
+            echo "<tr><td colspan='5'>Aucun résultat trouvé.</td></tr>";
+        }
+        ?>
+    </table>
+    <style>
+    th {
+        border: 1px solid black;
+        padding: 5px;
+    }
+</style>
+</body>
+</html>
