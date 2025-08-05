76ef67fa4706955862a2a7c555420e6948a05636
mathis-fournier <mathis.fournier@laplateforme.io>
2025-08-05 09:45:25 +0200

    job 6&7

---
+++ jour10/job07/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$servname = "localhost"; $user = "root"; $pass = ""; $dbname = "jour09";
+
+$conn = new mysqli($servname, $user, $pass, $dbname);
+$sql = "SELECT SUM(superficie) AS superficie_totale FROM etage";
+$result = $conn->query($sql);
+?>
+    <table>
+        <tr >
+            <th>Superficie Totale</th>
+        </tr>
+        <?php
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row["superficie_totale"] . "</td>";
+                echo "</tr>";
+            }
+        } else {
+            echo "<tr><td colspan='5'>Aucun résultat trouvé.</td></tr>";
+        }
+        ?>
+    </table>
+    <style>
+    th {
+        border: 1px solid black;
+        padding: 5px;
+    }
+</style>
+</body>
+</html>
